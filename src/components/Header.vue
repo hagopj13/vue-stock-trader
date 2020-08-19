d0712ac508312bea9c1071362493ddcf2dbcae87
Hagop Jamkojian <hagopj13@gmail.com>
2020-08-19 19:26:39 +0200

    Add currency filter

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,7 +13,7 @@
             <a>Stocks</a>
           </router-link>
         </ul>
-        <strong class="navbar-text navbar-right">Funds: {{ funds }}</strong>
+        <strong class="navbar-text navbar-right">Funds: {{ funds | currency }}</strong>
         <ul class="nav navbar-nav navbar-right">
           <li>
             <a href="#">End Day</a>
--- src/main.js
+++ src/main.js
@@ -9,6 +9,10 @@ Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
 
+Vue.filter('currency', (value) => {
+  return '$' + value.toLocaleString();
+});
+
 const router = new VueRouter({
   routes,
   mode: 'history',
